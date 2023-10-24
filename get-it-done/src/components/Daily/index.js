039ad9b03797d8c9b374d71ac9d7a32e33215da2
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-10-23 19:49:41 -0700

    moved app

---
+++ get-it-done/src/components/Daily/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+import Form from '../Form'
+
+function Daily() {
+    return (
+        <div className='Main'>
+            <h2>Daily Tasks
+                <Form />
+                {/* Todo List */}
+                {/* Key */}
+                {/* Author */}
+            </h2>
+        </div>
+    )
+}
+
+export default Daily
