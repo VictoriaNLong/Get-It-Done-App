db03f5307090ff4cbd0eb18c2b5e6953d781e897
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-10-28 14:12:43 -0700

    User route

--- Back End/routes/users.js
+++ Back End/routes/users.js
@@ -1,3 +1,8 @@
+const {getUserInfo, updateUser} = require('../controllers/user')
+
 const router = require("express").Router();
+
+router.get('/me', getUserInfo)
+router.put('/me', updateUser)
 
 module.exports = router
